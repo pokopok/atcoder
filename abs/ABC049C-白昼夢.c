eb39dcb7c671ee0bbeaf51c7f1dd20ae2a1673e5
pokopok <301lv.hjtm-pq@hitokura.net>
2022-07-14 20:40:59 +0900

    create ABC049C

---
+++ abs/ABC049C-白昼夢.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char s, t;
+    int len;
+    int i;
+
+    scanf("%s", &s);
+    i = 0;
+    len = strlen(&s);
+
+    
+
+    return (0);
+}
