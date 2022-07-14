af64af438a0451972e649372c79af3189858eeeb
pokopok <301lv.hjtm-pq@hitokura.net>
2022-07-14 18:50:06 +0900

    modify ABC088B

--- abs/ABC088B - Card Game for Two.c
+++ abs/ABC088B - Card Game for Two.c
@@ -1,3 +1,6 @@
+// 未完成
+// 大きい順への並べ替えがうまくいっていない
+
 #include <stdio.h>
 
 int main()
@@ -7,37 +10,49 @@ int main()
     int a[101];
     int Alice,Bob;
 
+    for(i=0; i<n; i++)
+    {
+        a[i] = 0;
+    }
+
     scanf("%d", &n);
     for (i=0; i<n; i++)
     {
-        scanf("%d", tmp);
+        scanf("%d", &tmp);
         j = 0;
         while(j <= i)
         {
             if(tmp > a[j])
             {
-                k = i + 1;
-                while(k >= j)
+                k = i;
+                while(k >= j && a[k] > 0)
                 {
-                    a[k] = a[k-1];
+                    a[k + 1] = a[k];
                     k--;
                 }
                 a[j] = tmp;
+                printf("%d", j);
+                break;
             }
-            if(j = i)
-                a[i] = tmp;
             j++;
         }
+    }
+    i=0;
+    while(i<n)
+    {
+        printf("%d\n", a[i]);
+        i++;
     }
     i = 0;
     Alice = 0;
     Bob = 0;
-    while(i <= n)
+    while(i < n)
     {
         Alice += a[i];
+        i++;
         if(i < n)
-            Bob += a[i+1];
-        i = i + 2;
+            Bob += a[i];
+        i++;
     }
     printf("%d", Alice - Bob);
 }
